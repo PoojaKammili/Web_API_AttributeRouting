4b8269a0c8fa41aec603dbd6d411dd62b8d68078
PoojaKammili <kammilipooja235@gmail.com>
2025-12-24 18:12:59 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_API_AttributeRouting.Services;
+using Web_API_AttributeRouting.Models;
+
+namespace Web_API_AttributeRouting.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeService _service;
+        public EmployeeController(EmployeeService service) { _service = service; }
+        //create
+        [HttpPost("Create")]
+        public IActionResult Create([FromBody] Employee employee)
+        {
+            var emp = _service.CreateEmployee(employee);
+            return CreatedAtAction(nameof(GetById), new { Id = emp.Id }, emp);
+        }
+
+        //addmultiple 
+        [HttpPost("CreateMultiple")]
+        public IActionResult CreateMultiple([FromBody] List<Employee> employees)
+        {
+            foreach (var emp in employees)
+            {
+                _service.CreateEmployee(emp);
+            }
+            return Ok("Employees added successfully");
+        }
+        //getall
+        [HttpGet("GetAll")]
+        public IActionResult Get()
+        {
+            var emp = _service.GetEmployees();
+            if (emp == null || emp.Count == 0)
+            {
+                return BadRequest("No Employee Found");
+            }
+            else
+            {
+                return Ok(emp);
+            }
+        }
+        //getbyid
+        [HttpGet("GetById")]
+        public IActionResult GetById([FromQuery] int id)
+        {
+            var emp = _service.GetEmployeeById(id);
+            if (emp == null)
+            {
+                return BadRequest($"No Employee found with id : {id}");
+            }
+            else
+            {
+                return Ok(emp);
+            }
+        }
+        //getbydept
+        [HttpGet("GetByDept")]
+        public IActionResult GetByDept([FromQuery] string department)
+        {
+            var emp = _service.GetEmployeeByDept(department);
+            if(emp == null && emp.Count == 0)
+            {
+                return BadRequest($"No employee found with department : {department}");
+            }
+            return Ok(emp);
+        }
+        //update
+        [HttpPut("Update")]
+
+        public IActionResult Update([FromQuery]int id,[FromBody]Employee employee)
+        {
+            var emp = _service.UpdateEmployee(employee.Id);
+            if (emp == null)
+            {
+                return NotFound("Employee not found");
+            }
+            emp.Name = employee.Name;
+            emp.Department = employee.Department;
+            emp.Salary = employee.Salary;
+            return Ok(emp);
+        }
+        //delete
+        [HttpDelete("Delete")]
+        public IActionResult Delete([FromQuery] int id)
+        {
+            var emp = _service.GetEmployeeById(id);
+            if (emp.Id == null)
+            {
+                return BadRequest("No student found");
+                
+            }
+            _service.DeleteEmployee(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Services/EmployeeService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_API_AttributeRouting.Models;
+    namespace Web_API_AttributeRouting.Services
+{
+    public class EmployeeService
+    {
+        static List<Employee> employees = new List<Employee>();
+        static int Id = 1;
+       public EmployeeService() { }
+        //create
+        public Employee CreateEmployee(Employee employee)
+        {
+            employee.Id = Id++;
+            employees.Add(employee);
+            return employee;
+
+        }
+        //getall
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+        //getbyid
+        public Employee GetEmployeeById(int id)
+        {
+            return employees.FirstOrDefault(e=>e.Id==id);
+        }
+        //getbydept
+        public List<Employee> GetEmployeeByDept(string department)
+        {
+            return employees.Where(e=>e.Department ==department).ToList();
+        }
+        //update
+        public Employee UpdateEmployee(int id)
+        {
+            return employees.FirstOrDefault(e => e.Id == id);
+        }
+        //delete
+        public void DeleteEmployee(int id)
+        {
+            var employee = employees.FirstOrDefault(e => e.Id == id);
+            if (employee != null)
+            {
+                employees.Remove(employee);
+            }
+        }
+    }
+}
